a147747c69c67b3d8ea6fbcf7fdd7c9ccde76032
chanchanbayub <chanchanbayub@gmail.com>
2024-07-31 09:24:38 +0700

    merubah status penderekan di admin/data penindakan

--- app/Controllers/Admin/DataPenindakan.php
+++ app/Controllers/Admin/DataPenindakan.php
@@ -488,7 +488,7 @@ class DataPenindakan extends BaseController
                     $this->bapModel->update($noBap->id, [
                         'id' => $noBap->id,
                         'bap_id' => $bap_id,
-                        'status_bap_id' => 5
+                        'status_bap_id' => 3
                     ]);
                 } else {
                     $this->bapModel->update($noBap->id, [
@@ -949,14 +949,14 @@ class DataPenindakan extends BaseController
                             $this->bapModel->update($noBap->id, [
                                 'id' => $noBap->id,
                                 'bap_id' => $bap_id,
-                                'status_bap_id' => 5
+                                'status_bap_id' => 4
                             ]);
                         }
                     } else {
                         $this->bapModel->update($noBap->id, [
                             'id' => $noBap->id,
                             'bap_id' => $bap_id,
-                            'status_bap_id' => 5
+                            'status_bap_id' => 3
                         ]);
                     }
                 } else {
